618d54f987e7e27dff06be742c658e08f6056085
Hemiah <309595700@qq.com>
2017-05-17 11:56:25 +0800

    add 7

---
+++ 7.模块共享引用/Bucky.js
@@ -0,0 +1,5 @@
+var bucky = require('./movies');
+
+bucky.favMovies = "肖申克救赎!";
+
+console.log(bucky.favMovies);
---
+++ 7.模块共享引用/Emily.js
@@ -0,0 +1,7 @@
+var emily = require('./movies');
+
+console.log(emily.favMovies);
+
+emily.favMovies = "卧虎藏龙";
+
+// 对象的引用
